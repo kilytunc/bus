588a04595a273748993dddc559e0446847c1b61b
MehmetAkifTUNC <kilyneu@gmail.com>
2020-11-25 14:27:36 +0100

    newbranch

--- src/test/java/akif_hello.java
+++ src/test/java/akif_hello.java
@@ -1,6 +1,8 @@
 public class akif_hello {
     public static void main(String[] args) {
         System.out.println("hello from Akifs own class");
+
+        //second commit to try collaboroter self merge
     }
 
 }
